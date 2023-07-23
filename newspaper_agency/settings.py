0b9dc31b11915bbf613b290d860a84c8b310a9c2
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-07-23 16:53:05 +0300

    Changes were made to settings.py, README.md, .html files

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-w(uplu9m7hj8%40rxh1&*dnu3uhne45-d9ec7#9h8o=@2u9umc")
+SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
